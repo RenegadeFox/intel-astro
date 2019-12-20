9a89795a5998e45d47d1bc88d938defd6996fe43
rootr <martincox99@me.com>
2019-12-19 20:49:52 -0800

    Add header component

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    Intel Astro
+  </header>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator"
+
+@Component
+export default class BaseHeader extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+  font-size: 64px;
+  font-weight: lighter;
+  font-family: "Roboto", sans-serif;
+}
+</style>
