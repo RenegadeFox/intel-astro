8d51b4dfc0d95f6607659b113a49893875bc151f
rootr <martincox99@me.com>
2019-12-19 20:06:42 -0800

    Add getState helper function

---
+++ src/assets/helpers.ts
@@ -0,0 +1,81 @@
+import { ApiCity } from "@/types/City"
+
+interface States<TValue> {
+  [id: string]: TValue
+}
+
+const states: States<string> = {
+  Alabama: "AL",
+  Alaska: "AK",
+  "American Samoa": "AS",
+  Arizona: "AZ",
+  Arkansas: "AR",
+  California: "CA",
+  Colorado: "CO",
+  Connecticut: "CT",
+  Delaware: "DE",
+  "District Of Columbia": "DC",
+  "Federated States Of Micronesia": "FM",
+  Florida: "FL",
+  Georgia: "GA",
+  Guam: "GU",
+  Hawaii: "HI",
+  Idaho: "ID",
+  Illinois: "IL",
+  Indiana: "IN",
+  Iowa: "IA",
+  Kansas: "KS",
+  Kentucky: "KY",
+  Louisiana: "LA",
+  Maine: "ME",
+  "Marshall Islands": "MH",
+  Maryland: "MD",
+  Massachusetts: "MA",
+  Michigan: "MI",
+  Minnesota: "MN",
+  Mississippi: "MS",
+  Missouri: "MO",
+  Montana: "MT",
+  Nebraska: "NE",
+  Nevada: "NV",
+  "New Hampshire": "NH",
+  "New Jersey": "NJ",
+  "New Mexico": "NM",
+  "New York": "NY",
+  "North Carolina": "NC",
+  "North Dakota": "ND",
+  "Northern Mariana Islands": "MP",
+  Ohio: "OH",
+  Oklahoma: "OK",
+  Oregon: "OR",
+  Palau: "PW",
+  Pennsylvania: "PA",
+  "Puerto Rico": "PR",
+  "Rhode Island": "RI",
+  "South Carolina": "SC",
+  "South Dakota": "SD",
+  Tennessee: "TN",
+  Texas: "TX",
+  Utah: "UT",
+  Vermont: "VT",
+  "Virgin Islands": "VI",
+  Virginia: "VA",
+  Washington: "WA",
+  "West Virginia": "WV",
+  Wisconsin: "WI",
+  Wyoming: "WY"
+}
+
+export default function(cityObj: ApiCity): string {
+  // Holds the state/country
+  const stateOrCountry = cityObj.name.split(", ")[1]
+
+  // Check if city is in a U.S. state
+  if (cityObj.c === "US") {
+    // return the state abbreviation
+    return states[stateOrCountry]
+  }
+
+  // Otherwise, just return the country
+  return stateOrCountry
+}
