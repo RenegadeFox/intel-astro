5c5bf9d3194789cd08159d20aff696f4414cda5b
rootr <martincox99@me.com>
2019-12-19 20:49:17 -0800

    Add basic template

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <base-header class="header" />
+
+    <base-sub-header class="sub-header" />
+
+    <router-view class="page-content-container" />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator"
+import BaseHeader from "@/components/BaseHeader.vue"
+import BaseSubHeader from "@/components/BaseSubHeader.vue"
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@Component({
+  components: {
+    BaseHeader,
+    BaseSubHeader
   }
-}
-</style>
+})
+export default class Layout extends Vue {}
+</script>
+
+<style lang="scss"></style>
