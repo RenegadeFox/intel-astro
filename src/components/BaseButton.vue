6e73392437936faf3489909a0b29fb8f1a0bdcd4
rootr <martincox99@me.com>
2019-12-19 20:59:10 -0800

    Add base button component
    Closes issue #6

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button v-bind="$attrs" v-on="listeners" class="button">{{ text }}</button>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator"
+
+@Component
+export default class BaseSubHeader extends Vue {
+  @Prop({ required: true, type: String }) text!: string
+
+  get listeners() {
+    return { ...this.$listeners }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
