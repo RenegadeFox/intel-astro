df81a8c458c980988aa07aac1814081648d33fef
rootr <martincox99@me.com>
2019-12-19 20:49:44 -0800

    Add sub-header component

---
+++ src/components/BaseSubHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <p class="text-faded">Easily get up to date astronomical weather data</p>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator"
+
+@Component
+export default class BaseSubHeader extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+p {
+  text-align: center;
+  font-size: 20px;
+}
+</style>
