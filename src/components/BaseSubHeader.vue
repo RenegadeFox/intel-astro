0f994a400c32fa0fd6ddc188cb7a4912a6f52179
rootr <martincox99@me.com>
2019-12-21 13:23:16 -0800

    Add content to astro-data message

--- src/components/BaseSubHeader.vue
+++ src/components/BaseSubHeader.vue
@@ -1,14 +1,37 @@
 <template>
-  <p class="text-faded" v-html="getPageMessage"></p>
+  <p class="text-faded" v-html="pageMessage"></p>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator"
-import { Getter } from "vuex-class"
+import { Getter, State } from "vuex-class"
+import { City } from "@/types/city"
 
 @Component
 export default class BaseSubHeader extends Vue {
   @Getter("getPageMessage") getPageMessage!: string
+  @State location!: City
+  @State dateRange!: Array<Date>
+
+  get pageMessage(): string {
+    const startDate = `${this.dateRange[0].getDate()} ${this.dateRange[0].toLocaleString(
+      "default",
+      { month: "short" }
+    )}`
+    const endDate = `${this.dateRange[
+      this.dateRange.length - 1
+    ].getDate()} ${this.dateRange[this.dateRange.length - 1].toLocaleString(
+      "default",
+      {
+        month: "short"
+      }
+    )}`
+
+    return this.getPageMessage
+      .replace("{city}", `${this.location.city}, ${this.location.state}`)
+      .replace("{startDate}", startDate)
+      .replace("{endDate}", endDate)
+  }
 }
 </script>
 
