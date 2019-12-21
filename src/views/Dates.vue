f8e98fe4457ec396c7d3a13da3906a44ed4142ed
rootr <martincox99@me.com>
2019-12-21 12:33:01 -0800

    Add dates page

--- src/views/Dates.vue
+++ src/views/Dates.vue
@@ -7,6 +7,7 @@
 <script lang="ts">
 import axios from "axios"
 import { Component, Vue } from "vue-property-decorator"
+import { Action } from "vuex-class"
 import Calendar from "@/components/Calendar.vue"
 import { DateRange } from "@/components/Calendar.vue"
 
@@ -16,13 +17,19 @@ import { DateRange } from "@/components/Calendar.vue"
   }
 })
 export default class App extends Vue {
+  @Action("updateDateRange") updateDateRange!: Function
+  @Action("updatePage") updatePage!: Function
+
   userSelectedDate({ startDate, endDate, allDates }: DateRange): void {
     if (allDates.length > 1) {
       // Add to Vuex store
+      this.updateDateRange(allDates)
       // Redirect to astro data page
+      this.$router.push({ name: "astro-data" })
     } else if (allDates.length < 2) {
       // Otherwise, if only first date is selected
       // Change the subheader to say "Select end date"
+      this.updatePage("endDate")
     }
   }
 }
