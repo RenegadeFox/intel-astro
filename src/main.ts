c7cb378ad76c7dabb60b546fe2a6f34d602b4016
Martin <martincox99@me.com>
2019-12-22 08:15:07 -0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,10 @@
 import Vue from "vue"
 import App from "./App.vue"
-import { initializeApp } from "firebase"
 import "./registerServiceWorker"
 import router from "./router"
 import store from "./store"
 
 Vue.config.productionTip = false
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB2xbebdE64zWt556mtSuwY_Dy1TUx-Sr4",
-  authDomain: "intel-apps.firebaseapp.com",
-  databaseURL: "https://intel-apps.firebaseio.com",
-  projectId: "intel-apps",
-  storageBucket: "intel-apps.appspot.com",
-  messagingSenderId: "1062671776788",
-  appId: "1:1062671776788:web:ac2334c2a6fb0c6f757b01",
-  measurementId: "G-MZGMJQJ4DD"
-}
-
-initializeApp(firebaseConfig)
 
 new Vue({
   router,
